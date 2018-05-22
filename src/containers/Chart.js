c3950b1f051b1c085946c122f163d9210c16d828
hcaula <henrique.caula@hotmail.com>
2018-05-22 03:37:29 -0300

    Adds force graph to Chart component

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 import { select } from 'd3-selection'
 import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force'
+import { drag } from 'd3-drag'
+import { event } from 'd3'
 
 class BarChart extends Component {
     constructor(props) {
@@ -19,78 +21,91 @@ class BarChart extends Component {
     }
 
     createBarChart() {
+        let graph = this.relations;
+
         const svg = select("svg");
         const width = +svg.attr("width");
         const height = +svg.attr("height");
+
+        /* Removing the week affinities */
+        graph.relations = graph.relations.sort((a,b) => b.affinity - a.affinity);
+        graph.relations = graph.relations.slice(0, graph.relations.length/2);
+
+        graph.relations = graph.relations.map(rel => {
+            let ret = rel;
+            ret.value = rel.affinity;
+            return ret;
+        });
 
         const simulation = forceSimulation()
             .force("link", forceLink().id(function (d) { return d.id; }))
             .force("charge", forceManyBody())
             .force("center", forceCenter(width / 2, height / 2));
 
-        console.log(this.relations);
+        const link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(graph.relations)
+            .enter().append("line")
+            .attr("stroke-width", function (d) { return Math.sqrt(d.affinity); });
 
-        // d3.json("miserables.json", function (error, graph) {
-        //     if (error) throw error;
+        const node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(graph.users)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("fill", function (d) { return 'blue'; })
+            .call(drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
 
-        //     var link = svg.append("g")
-        //         .attr("class", "links")
-        //         .selectAll("line")
-        //         .data(graph.links)
-        //         .enter().append("line")
-        //         .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+        node.append("title")
+            .text(function (d) { return d.display_name; });
 
-        //     var node = svg.append("g")
-        //         .attr("class", "nodes")
-        //         .selectAll("circle")
-        //         .data(graph.nodes)
-        //         .enter().append("circle")
-        //         .attr("r", 5)
-        //         .attr("fill", function (d) { return color(d.group); })
-        //         .call(d3.drag()
-        //             .on("start", dragstarted)
-        //             .on("drag", dragged)
-        //             .on("end", dragended));
+        simulation
+            .nodes(graph.users)
+            .on("tick", ticked);
 
-        //     node.append("title")
-        //         .text(function (d) { return d.id; });
+        simulation.force("link")
+            .links(graph.relations);
 
-        //     simulation
-        //         .nodes(graph.nodes)
-        //         .on("tick", ticked);
+        function ticked() {
+            link
+                .attr("x1", function (d) { return findUser(d.user_1).x; })
+                .attr("y1", function (d) { return findUser(d.user_1).y; })
+                .attr("x2", function (d) { return findUser(d.user_2).x; })
+                .attr("y2", function (d) { return findUser(d.user_2).y; });
 
-        //     simulation.force("link")
-        //         .links(graph.links);
+            node
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; });
+        }
 
-        //     function ticked() {
-        //         link
-        //             .attr("x1", function (d) { return d.source.x; })
-        //             .attr("y1", function (d) { return d.source.y; })
-        //             .attr("x2", function (d) { return d.target.x; })
-        //             .attr("y2", function (d) { return d.target.y; });
+        const findUser = function(id) {
+            for (let i in graph.users) {
+                const user = graph.users[i];
+                if(user._id == id) return user;
+            }
+        }
 
-        //         node
-        //             .attr("cx", function (d) { return d.x; })
-        //             .attr("cy", function (d) { return d.y; });
-        //     }
-        // });
+        function dragstarted(d) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
 
-        // function dragstarted(d) {
-        //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        //     d.fx = d.x;
-        //     d.fy = d.y;
-        // }
+        function dragged(d) {
+            d.fx = event.x;
+            d.fy = event.y;
+        }
 
-        // function dragged(d) {
-        //     d.fx = d3.event.x;
-        //     d.fy = d3.event.y;
-        // }
-
-        // function dragended(d) {
-        //     if (!d3.event.active) simulation.alphaTarget(0);
-        //     d.fx = null;
-        //     d.fy = null;
-        // }
+        function dragended(d) {
+            if (!event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
     }
 
     render() {
