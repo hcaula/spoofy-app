222d82307b82f24421541beeb5299b9801d43d18
hcaula <henrique.caula@hotmail.com>
2018-06-08 08:20:42 -0300

    Stylize dashboard page

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../styles/Dashboard.css';
+
 import Error from '../components/Error';
 import Waiting from '../components/Waiting';
 
@@ -9,6 +11,7 @@ export class Dashboard extends Component {
         this.user = this.props.user;
         this.token = this.props.token;
         this.isNew = this.props.isNew;
+        this.users = [];
     }
 
     state = { ready: false, error: false };
@@ -18,7 +21,16 @@ export class Dashboard extends Component {
             this.requestTop(this.token)
                 .then(res => {
                     if (res.status != 200) this.setState({ error: res });
-                    else this.setState({ ready: true });
+                    else {
+                        this.requestUsers(this.token)
+                            .then(res => {
+                                if (res.status != 200) this.setState({ error: res });
+                                else {
+                                    this.users = res.users;
+                                    this.setState({ ready: true });
+                                }
+                            })
+                    }
                 });
         } else this.setState({ ready: true });
     }
@@ -35,15 +47,30 @@ export class Dashboard extends Component {
         return body;
     };
 
+    requestUsers = async (access_token) => {
+        const api = process.env.REACT_APP_SPOOFYAPI;
+        const response = await fetch(api + '/api/v2/all/users', {
+            headers: { 'access_token': access_token }
+        });
+
+        const body = await response.json();
+        body.status = response.status;
+        return body;
+    };
+
     render() {
         if (this.state.error) return <Error error={this.state.error} />;
         else if (!this.state.ready) return <Waiting message="Hi! We're requesting your top tracks" />;
         else return (
-            <div>
-                <h1> Dashboard </h1>
-                <h2> Name: {this.user.display_name}</h2>
-                <h2> ID: {this.user._id}</h2>
-                <h2> Token: {this.token} </h2>
+            <div className="Dashboard">
+                <div className="DashboardTitle">
+                    <h1> thank you for joining, {this.user.display_name} </h1>
+                    <h2> please, select the people you believe have similar taste in music with you </h2>
+                    <p> (soon, we'll have a graph visualization of your friends. for now, we just want to do some tests) </p>
+                </div>
+
+                <div className="DashboardList">
+                </div>
             </div>
         );
     }
