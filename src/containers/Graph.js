f39467aff3f6e3d6baf7fccd31a83c153d51bef0
hcaula <henrique.caula@hotmail.com>
2018-06-09 01:32:50 -0300

    Creates genre nodes array

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import '../styles/Graph.css';
 
+const searchByField = function(value, param, array) {
+    let index = -1;
+    array.forEach((el, i) => {
+        if (el[param] === value) {
+            index = i;
+            return;
+        }
+    });
+    return index;
+}
+
 class Graph extends Component {
     constructor(props) {
         super(props)
@@ -11,16 +22,29 @@ class Graph extends Component {
         this.users = this.props.users;
     }
 
-    componentDidMount() {
+    init() {
+        this.getGenreNodes();
         this.createGraph();
     }
 
-    componentDidUpdate() {
-        this.createGraph();
+    componentDidMount() {this.init()}
+    componentDidUpdate() {this.init()}
+
+    getGenreNodes() {
+        this.genreNodes = [];
+        this.users.forEach(u => {
+            u.genres.forEach(g => {
+                g.id = g._id;
+                delete g._id;
+                const index = searchByField(g.name, 'name', this.genreNodes);
+                if (index > -1) this.genreNodes[index].weight += g.weight;
+                else this.genreNodes.push(g);
+            });
+        });
+        this.genreNodes = this.genreNodes.sort((a, b) => b.weight - a.weight);
     }
 
     createGraph() {
-        console.log(this.users);
     }
 
     render() {
