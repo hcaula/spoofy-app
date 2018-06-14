fd4a9402287db27d9b79173353a48b99eb9c83f3
hcaula <henrique.caula@hotmail.com>
2018-06-14 11:17:32 -0300

    Adds chart

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+import { select, selectAll } from 'd3-selection'
+import { forceSimulation, forceLink, forceManyBody, forceCenter } from 'd3-force'
+import { drag } from 'd3-drag'
+import { event } from 'd3'
+
 import '../styles/App.css';
 import '../styles/Graph.css';
 
@@ -25,6 +30,8 @@ class Graph extends Component {
     init() {
         this.setGenreNodes();
         this.setUserNodes();
+
+        this.nodes = this.genreNodes.concat(this.userNodes)
         this.setLinks();
         this.createGraph();
     }
@@ -63,26 +70,108 @@ class Graph extends Component {
         this.links = [];
         this.users.forEach(u => {
             u.genres.forEach(g => {
-                const index = searchByField(g.name, 'name', this.genreNodes);
-                let id;
-                if (index > -1) id = this.genreNodes[index].id;
-                else id = g._id;
-                this.links.push({
-                    source: u._id,
-                    target: id,
-                    weight: g.weight,
-                    name: g.name
-                });
+                if (g.weight > 0) {
+                    const index = searchByField(g.name, 'name', this.genreNodes);
+                    let id;
+                    if (index > -1) id = this.genreNodes[index].id;
+                    else id = g._id;
+                    this.links.push({
+                        source: u._id,
+                        target: id,
+                        weight: g.weight,
+                        name: g.name
+                    });
+                }
             });
         });
     }
 
     createGraph() {
+        const svg = select('svg');
+        const width = +svg.attr("width");
+        const height = +svg.attr("height");
+
+        const simulation = forceSimulation()
+            .force("link", forceLink().id(d => d.id).distance(200))
+            .force("charge", forceManyBody())
+            .force("center", forceCenter(width / 2, height / 2));
+
+        const link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(this.links)
+            .enter().append("line")
+            .attr("stroke-width", d => Math.sqrt(d.weight));
+
+        simulation
+            .nodes(this.nodes)
+            .on("tick", ticked)
+
+        simulation.force("link")
+            .links(this.links)
+
+        const node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(this.nodes)
+            .enter()
+            .append('circle')
+            .attr('r', g => g.weight ? g.weight * 2 : 5 * 2)
+            .attr('fill', g => g.type === 'genre' ? 'blue' : 'red')
+            .call(drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended))
+
+        // const text = svg.append("g")
+        // .attr("class", "texts")
+        // .selectAll("circle")
+        // .data(this.nodes)
+        // .enter()
+        // .append("text")
+        // .attr("x", d => d.x)
+        // .attr("y", d => d.y)
+        // .text(d => d.name)
+
+        node.append("title")
+            .text(d => d.id);
+
+        function ticked() {
+            link
+                .attr("x1", function (d) { return d.source.x; })
+                .attr("y1", function (d) { return d.source.y; })
+                .attr("x2", function (d) { return d.target.x; })
+                .attr("y2", function (d) { return d.target.y; });
+
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        }
+
+        function dragstarted(d) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = event.x;
+            d.fy = event.y;
+        }
+
+        function dragended(d) {
+            if (!event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
     }
 
     render() {
+        const width = 1000;
+        const height = 1000;
         return (
-            <svg ref={node => this.node = node} className="Graph"></svg>
+            <svg ref={node => this.node = node} width={width} height={height} className="Graph"></svg>
         )
     }
 }
